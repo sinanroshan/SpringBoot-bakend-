d2dd2fbaeade761a24313c73e0b3dce80adb90b5
sinan roshan <sinanroshan2233@gmail.com>
2022-01-04 11:53:32 +0530

    company data update done

--- src/main/java/net/springboot/backend/controller/CompanyDataContrtoller.java
+++ src/main/java/net/springboot/backend/controller/CompanyDataContrtoller.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.springboot.backend.model.CompanyData;
@@ -24,5 +26,11 @@ public class CompanyDataContrtoller {
 			GetCompany(){
 		return companyDataRrepository.getCompany();
 	}
+
+	@RequestMapping(path = "EditProduct/",method = RequestMethod.POST)
+	public String EditProduct(@RequestBody CompanyData data) {
+			companyDataRrepository.updateData(data.getName(),data.getAddress1(),data.getAddres2(),data.getPhone(),data.getEmail(),data.getWebsite(),data.getGstNo(),data.getCity(),data.getState(),data.getCountry(),data.getBank(),data.getBankBranch(),data.getBankIfce(),data.getBankAccNO(),data.getLogo());
+			return "Updated";
+			}	
     
 }
--- src/main/java/net/springboot/backend/repository/CompanyDataRrepository.java
+++ src/main/java/net/springboot/backend/repository/CompanyDataRrepository.java
@@ -2,8 +2,12 @@ package net.springboot.backend.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+import javax.xml.crypto.Data;
+
 import org.hibernate.annotations.Any;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import net.springboot.backend.model.CompanyData;
@@ -12,4 +16,9 @@ public interface CompanyDataRrepository extends JpaRepository<CompanyData, Any>{
     
     @Query(value="SELECT * FROM company", nativeQuery = true)
 	public CompanyData getCompany();
+
+    @Modifying
+	@Transactional
+    @Query(value="Update company SET cmp_add1=:address1,cmp_add2=:address2,cmp_phone=:phone,cmp_mail=:email,cmp_site=:website,cmp_gstno=:gstNo,cmp_city=:city,cmp_state=:state,cmp_country=:country,cmp_bname=:bank,cmp_bbranch=:bankBranch,cmp_bifsc=:bankIfce=:cmp_baccountno=:bankAccNO,cmp_logo=:Logo    WHERE cmp_name =:name", nativeQuery = true)
+    public void updateData(String name,String address1,String address2,String phone,String email,String website,String gstNo,String city,String state,String country,String bank,String bankBranch,String bankIfce,String bankAccNO,String Logo);
 }
